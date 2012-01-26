66447768cd77f9aff6db3729a0de8b044fffb019
Vivien Barousse <barousse.vivien@gmail.com>
2012-01-26 22:01:38 +0100

    Add support for parametrized builds

--- lib/jkcli/jenkins/jenkins_client.rb
+++ lib/jkcli/jenkins/jenkins_client.rb
@@ -12,14 +12,22 @@ module Jkcli
         res = get '/api/json'
         res.json["jobs"] || []
       end
+      
+      def job(job_name)
+        res = get "/job/#{job_name}/api/json"
+        res.json
+      end
 
       def views
         res = get '/api/json'
         res.json["views"] || []
       end
 
-      def build(job_name)
-        get "/job/#{job_name}/build"
+      def build(job_name, options={})
+          url = "/job/#{job_name}/build"
+          url += "WithParameters" if options[:parameters] && options[:parameters].any?
+          post(url, options[:parameters] || {})
+        rescue RestClient::Found
       end
 
     end
